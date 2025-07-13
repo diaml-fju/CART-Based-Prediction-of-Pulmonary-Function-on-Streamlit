6654d42c2a17a7d8feae920871c5378e8d74d329
zac1227 <a0986710233@gmail.com>
2025-07-13 23:03:27 +0800

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -94,7 +94,7 @@ def show_predict_page():
            
 
             st.subheader("Decision Tree Visualization")
-            st.image("cart1.png")
+            st.image("cart2.png")
 
         except ValueError as e:
             st.error(f"An error occurred: {e}")
